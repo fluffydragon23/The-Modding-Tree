4a83e01e513f4bc26313ef6d3954553790914cd2
shythrice12@gmail.com <shythrice12@gmail.com>
2023-09-06 00:15:15 +0800

    endgame text

--- js/mod.js
+++ js/mod.js
@@ -62,11 +62,12 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
+  "Endgame: e13 points"
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("1e13"))
 }
 
 
